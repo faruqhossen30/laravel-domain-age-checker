42d39c986fa133087875d38937243f99eb5a3cac
Faruq Hossen <faruqhossen30@gmail.com>
2022-09-06 16:23:02 +0600

    fronted view added

---
+++ resources/views/frontend/layouts/footer.blade.php
@@ -0,0 +1,68 @@
+<footer>
+    <div class="footer-area py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-3 col-md-4 col-sm-3">
+                    <div class="footer-widget">
+                        <h5>About Us</h5>
+                        <p>Vestibulum quis risus sed nisl pellentesque aliquet et et lorem.</p>
+                        <p>Fusce nibh nisl, gravida nec ipsum eu, feugiat condimentum velit.</p>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-md-2 col-sm-3">
+                    <div class="footer-widget">
+                        <h6>Features</h6>
+                        <ul class="footer-menu">
+                            <li><a href="#">home</a></li>
+                            <li><a href="#">home</a></li>
+                            <li><a href="#">home</a></li>
+                            <li><a href="#">home</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-md-2 col-sm-3">
+                    <div class="footer-widget">
+                        <h6>Support</h6>
+                        <ul class="footer-menu">
+                            <li><a href="#">home</a></li>
+                            <li><a href="#">home</a></li>
+                            <li><a href="#">home</a></li>
+                            <li><a href="#">home</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-2 col-md-2 col-sm-3">
+                    <div class="footer-widget">
+                        <h6>Trending</h6>
+                        <ul class="footer-menu">
+                            <li><a href="#">home</a></li>
+                            <li><a href="#">home</a></li>
+                            <li><a href="#">home</a></li>
+                            <li><a href="#">home</a></li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-md-2 col-sm-3">
+                    <div class="footer-widget">
+                        <h6>Legal</h6>
+                        <ul class="footer-menu">
+                            <li><a href="#">home</a></li>
+                            <li><a href="#">home</a></li>
+                            <li><a href="#">home</a></li>
+                            <li><a href="#">home</a></li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</footer>
+<div class="copyright text-center my-4">
+    <div class="container">
+        <div class="row">
+            <div class="col-sm-12">
+                <span>Copyrights © 2022. All Rights Reserved by ThemeLuxury.</span>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/frontend/layouts/header.blade.php
@@ -0,0 +1,39 @@
+<header>
+    <div class="header-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-xl-12">
+                    <nav class="navbar navbar-expand-lg navbar-light">
+                        <div class="container-fluid">
+                            <a class="navbar-brand" href="{{route('homepage')}}">
+                                {{-- <img src="{{asset('frontend')}}/images/logo.png" alt=""> --}}
+                                Domain Age Checker
+                            </a>
+                            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                                aria-expanded="false" aria-label="Toggle navigation">
+                                <span class="navbar-toggler-icon"></span>
+                            </button>
+                            <div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                                <ul class="navbar-nav">
+                                    <li class="nav-item">
+                                        <a class="nav-link" aria-current="page" href="#">Home</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" aria-current="page" href="#">About</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" aria-current="page" href="#">Blog</a>
+                                    </li>
+                                    <li class="nav-item">
+                                        <a class="nav-link" aria-current="page" href="#">Contact</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
